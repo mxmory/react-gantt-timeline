29bb872e282fb854b4198422fe277095d5b1681b
Aidar <aidar@mircod.com>
2022-06-16 18:27:00 +0300

    WIP: nesting

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Stage, Layer, Line, Rect, Group, Text } from 'react-konva';
 import styles from './App.module.scss';
 import { range } from 'lodash';
-import { width, height, padding, initialData, ACTUAL_DATA } from './constants';
+import { width, height, padding, ACTUAL_DATA } from './constants';
 import { Sider } from './components/Sider/index';
 import { StageItemLine } from './components/StageItemLine';
 import { TaskItem } from './components/TaskItem';
 import { getStageProps } from './utils/funcs';
 import moment from 'moment';
+import { render } from 'react-dom';
+import { useRef } from 'react';
 
 const reduceStagesToShow = (data) => data.reduce((acc, stage) => ({ ...acc, [stage.id]: true }), {});
 
@@ -17,9 +19,64 @@ const App = () => {
     const [isTransforming, setIsTransforming] = useState(false);
     const [visibleStages, setVisibleStages] = useState(reduceStagesToShow(data));
 
+    const stageRef = useRef();
+    const containerRef = useRef();
+
+    const width = 200 * padding;
+
     const toggleStageCollapse = (stageId) => {
         setVisibleStages((prev) => ({ ...prev, [stageId]: !prev[stageId] }));
     };
+
+    const renderHeaderRows = (top, middle, bottom) => {
+        // let result = { top: [], middle: [], bottom: [] };
+        // let lastLeft = {};
+        // let currentTop = '';
+        // let currentMiddle = '';
+        // let currentBottom = '';
+        // let currentDate = null;
+        // let box = null;
+
+        // let start = this.props.currentday;
+        // let end = this.props.currentday + this.props.numVisibleDays;
+        const BUFFER_DAYS = 100;
+        const today = moment(Date.now());
+        const bufferPrevDate = moment(today).subtract(BUFFER_DAYS, 'days');
+        const bufferNextDate = moment(today).add(BUFFER_DAYS, 'days');
+
+        // for (let i = start - BUFFER_DAYS; i < end + BUFFER_DAYS; i++) {
+        //     //The unit of iteration is day
+        //     // currentDate = moment().add(i, 'days');
+        //     // if (currentTop != currentDate.format(this.getFormat(top, 'top'))) {
+        //     //     currentTop = currentDate.format(this.getFormat(top, 'top'));
+        //     //     box = this.getBox(currentDate, top, lastLeft.top);
+        //     //     lastLeft.top = box.left + box.width;
+        //     //     result.top.push(<HeaderItem key={i} left={box.left} width={box.width} label={currentTop} />);
+        //     // }
+        //     // if (currentMiddle != currentDate.format(this.getFormat(middle))) {
+        //     //     currentMiddle = currentDate.format(this.getFormat(middle));
+        //     //     box = this.getBox(currentDate, middle, lastLeft.middle);
+        //     //     lastLeft.middle = box.left + box.width;
+        //     //     result.middle.push(<HeaderItem key={i} left={box.left} width={box.width} label={currentMiddle} />);
+        //     // }
+        //     // if (currentBottom != currentDate.format(this.getFormat(bottom))) {
+        //     //     currentBottom = currentDate.format(this.getFormat(bottom));
+        //     //     box = this.getBox(currentDate, bottom, lastLeft.bottom);
+        //     //     lastLeft.bottom = box.left + box.width;
+        //     //     if (bottom == 'shorttime' || bottom == 'fulltime') {
+        //     //         result.bottom.push(this.renderTime(box.left, box.width, bottom, i));
+        //     //     } else {
+        //     //         result.bottom.push(<HeaderItem key={i} left={box.left} width={box.width} label={currentBottom} />);
+        //     //     }
+        //     // }
+        // }
+    };
+
+    useEffect(() => {
+        // renderHeaderRows();
+        containerRef.current.addEventListener('scroll', repositionStage);
+        repositionStage();
+    }, []);
 
     // const onGridStageDragEnd = (e) => {
     //     const {
@@ -126,6 +183,16 @@ const App = () => {
     //     selectShape(null);
     // };
 
+    function repositionStage() {
+        const stage = stageRef.current;
+        const container = containerRef.current;
+        var dx = container.scrollLeft - padding;
+        var dy = container.scrollTop - padding;
+        stage.container().style.transform = 'translate(' + dx + 'px, ' + dy + 'px)';
+        stage.x(-dx);
+        stage.y(-dy);
+    }
+
     const onDeselect = () => {
         if (!isTransforming) {
             selectShape(null);
@@ -133,21 +200,92 @@ const App = () => {
     };
 
     const getMonths = () => {
-        const start = moment('12-12-2018', 'MM-DD-YYYY');
-        const end = moment('12-12-2011', 'MM-DD-YYYY');
+        const start = moment('12-12-2011', 'MM-DD-YYYY');
+        const end = moment('12-12-2023', 'MM-DD-YYYY');
         let result = [];
 
         // console.log(start, end, end.isSameOrBefore(start, 'month'));
 
-        while (end.isSameOrBefore(start, 'month')) {
-            result = [...result, start.format('YYYY-MM')];
-            start.subtract(1, 'month');
-        }
+        // while (end.isSameOrBefore(start, 'month')) {
+        //     result = [...result, start.format('YYYY-MM')];
+        //     start.subtract(1, 'month');
+        // }
 
         return result.reverse();
     };
 
-    let monthDaysCount = 0;
+    const getDaysInMonth = (value) => {
+        return moment(value, 'YYYY-MM').daysInMonth();
+    };
+
+    const CoreStage = ({ stage, line }) => {
+        const { tasks, type, length, start_at, stages } = stage;
+        const { x, width } = getStageProps(stages);
+
+        return (
+            <StageItemLine
+                select={selectShape}
+                id={stage.id}
+                tasks={tasks}
+                line={line}
+                isSelected={selectedId === stage.id}
+                length={width}
+                start_at={x / padding}
+                type="core"
+                // onDragEnd={onGridStageDragEnd}
+                onDeselect={onDeselect}
+            />
+        );
+    };
+
+    const GridLineItem = ({ stage, currentLine }) => {
+        const { tasks, start_at, stages, length } = stage;
+
+        console.log(stage.id, currentLine);
+
+        console.log(tasks);
+        return (
+            <>
+                <StageItemLine
+                    select={selectShape}
+                    id={stage.id}
+                    tasks={tasks}
+                    line={currentLine}
+                    isSelected={selectedId === stage.id}
+                    length={length}
+                    start_at={start_at}
+                    // onDragEnd={onGridStageDragEnd}
+                    onDeselect={onDeselect}
+                />
+
+                {stages &&
+                    stages.map((s, idx) => {
+                        return <GridLineItem key={s.id} stage={s} currentLine={currentLine + idx + 1} />;
+                    })}
+
+                {tasks &&
+                    tasks.map((task, taskIdx) => {
+                        const { id, length, start_at, tasks = [] } = task;
+                        console.log('currLine', stages?.length ?? 0 + currentLine);
+                        return (
+                            <StageItemLine
+                                key={task.id}
+                                select={selectShape}
+                                id={id}
+                                tasks={tasks}
+                                line={4}
+                                isSelected={selectedId === id}
+                                length={length}
+                                start_at={start_at}
+                                // onDragEnd={onGridStageDragEnd}
+                                onDeselect={onDeselect}
+                                type="task"
+                            />
+                        );
+                    })}
+            </>
+        );
+    };
 
     return (
         <div className={styles.main}>
@@ -159,97 +297,119 @@ const App = () => {
                     visibleStages={visibleStages}
                 />
 
-                <div className={styles.grid}>
-                    <Stage
-                        width={width}
-                        height={height}
-                        onMouseUp={onDeselect}
-                        listening
-                        // onWheel={(e) => console.log(e)}
-                    >
-                        <Layer y={0}>
-                            <Line
-                                points={[0, 1 * padding - 0.5, width, 1 * padding - 0.5]}
-                                stroke="#aaa"
-                                strokeWidth={0.5}
-                            />
-                            <Line
-                                points={[0, 2 * padding - 0.5, width, 2 * padding - 0.5]}
-                                stroke="#aaa"
-                                strokeWidth={0.5}
-                            />
-                            {getMonths().map((item, index) => {
-                                const daysCount = moment(item, 'YYYY-MM').daysInMonth();
-                                const prevStages = [...getMonths().slice(0, index)];
-                                const prevStagesDaysCount = prevStages.reduce((acc, curr) => acc + curr, 0);
-                                console.log(prevStagesDaysCount);
-                                // TODO
-
-                                return (
-                                    <Group
-                                        key={item}
-                                        x={0}
-                                        y={0}
-                                        height={padding}
-                                        draggable={true}
-                                        dragBoundFunc={(pos) => {
-                                            return {
-                                                x: pos.x,
-                                                y: 0,
-                                            };
-                                        }}
-                                    >
-                                        <Rect
-                                            key={item}
-                                            width={padding * daysCount}
-                                            height={padding}
-                                            fill={'#ff00ff'}
-                                            opacity={0.5}
-                                            stroke="#ccc"
-                                            strokeWidth={0.5}
-                                        />
-                                        <Text
-                                            width={padding * daysCount}
-                                            height={padding}
-                                            text={
-                                                index *
-                                                moment(
-                                                    getMonths()[index - 1] || getMonths()[0],
-                                                    'YYYY-MM'
-                                                ).daysInMonth()
-                                            }
-                                            align="center"
-                                            verticalAlign="middle"
-                                        />
-                                    </Group>
-                                );
-                            })}
-                        </Layer>
-                        <Layer y={padding * 2}>
-                            {range(Math.round(width / padding)).map((n) => (
+                <div className={styles.grid} ref={containerRef}>
+                    <div className={styles.innerGridContainer}>
+                        <Stage
+                            width={window.innerWidth + padding * 2}
+                            height={window.innerHeight + padding * 2}
+                            ref={stageRef}
+                            onMouseUp={onDeselect}
+                            listening
+                            // onWheel={(e) => console.log(e)}
+                        >
+                            <Layer y={0}>
                                 <Line
-                                    key={'_vert_line_' + n}
-                                    points={[Math.round(n * padding) + 0.5, 0, Math.round(n * padding) + 0.5, height]}
+                                    points={[0, 1 * padding - 0.5, width, 1 * padding - 0.5]}
                                     stroke="#aaa"
                                     strokeWidth={0.5}
                                 />
-                            ))}
-
-                            {range(Math.round(height / padding)).map((n) => (
                                 <Line
-                                    key={'_horz_line_' + n}
-                                    points={[
-                                        0,
-                                        Math.round((n + 1) * padding) - 0.5,
-                                        width,
-                                        Math.round((n + 1) * padding) - 0.5,
-                                    ]}
+                                    points={[0, 2 * padding - 0.5, width, 2 * padding - 0.5]}
                                     stroke="#aaa"
                                     strokeWidth={0.5}
                                 />
-                            ))}
+                                {getMonths().map((item, index) => {
+                                    const daysCount = getDaysInMonth(item);
+                                    const prevStages = [...getMonths().slice(0, index)];
+                                    const prevStagesDaysCount = prevStages.reduce((acc, curr) => {
+                                        return acc + getDaysInMonth(curr);
+                                    }, 0);
 
-                            {/* {data.map((stage, stageIdx) => {
+                                    return (
+                                        <Group key={item} x={prevStagesDaysCount * padding} y={0} height={padding}>
+                                            <Rect
+                                                key={item}
+                                                width={padding * daysCount}
+                                                height={padding}
+                                                fill="#fff"
+                                                opacity={0.5}
+                                                stroke="#ccc"
+                                                strokeWidth={0.5}
+                                            />
+                                            <Text
+                                                width={padding * daysCount}
+                                                height={padding}
+                                                text={moment(item).format('MMMM')}
+                                                align="center"
+                                                verticalAlign="middle"
+                                            />
+                                        </Group>
+                                    );
+                                })}
+                            </Layer>
+                            <Layer y={padding * 2}>
+                                {range(Math.round(width / padding)).map((n) => (
+                                    <Line
+                                        key={'_vert_line_' + n}
+                                        points={[
+                                            Math.round(n * padding) + 0.5,
+                                            0,
+                                            Math.round(n * padding) + 0.5,
+                                            height,
+                                        ]}
+                                        stroke="#aaa"
+                                        strokeWidth={0.5}
+                                    />
+                                ))}
+
+                                {range(Math.round(height / padding)).map((n) => (
+                                    <Line
+                                        key={'_horz_line_' + n}
+                                        points={[
+                                            0,
+                                            Math.round((n + 1) * padding) - 0.5,
+                                            width,
+                                            Math.round((n + 1) * padding) - 0.5,
+                                        ]}
+                                        stroke="#aaa"
+                                        strokeWidth={0.5}
+                                    />
+                                ))}
+
+                                {data.map((stage, stageIdx) => {
+                                    const { stages } = stage;
+                                    const prevStages = [...data.slice(0, stageIdx)];
+                                    const getPrevItems = (prevStages) => {
+                                        const resArr = prevStages.reduce((acc, { id, stages = [], tasks = [] }) => {
+                                            const all = [...tasks, ...stages];
+                                            const inner = getPrevItems(stages);
+
+                                            // console.log('prev prev', prevCount, stages);
+                                            return [...all, ...inner];
+                                        }, []);
+                                        return resArr;
+                                    };
+
+                                    const currentLine = stageIdx + getPrevItems(prevStages).length;
+
+                                    return (
+                                        <React.Fragment key={stage.id}>
+                                            <CoreStage stage={stage} line={currentLine} />
+
+                                            {stages.map((el, idx) => {
+                                                return (
+                                                    <GridLineItem
+                                                        key={el.id}
+                                                        stage={el}
+                                                        currentLine={currentLine + idx + 1}
+                                                    />
+                                                );
+                                            })}
+                                        </React.Fragment>
+                                    );
+                                })}
+
+                                {/* {data.map((stage, stageIdx) => {
                                 const { tasks } = stage;
 
                                 const prevStages = [...data.slice(0, stageIdx)];
@@ -293,8 +453,9 @@ const App = () => {
                                     </React.Fragment>
                                 );
                             })} */}
-                        </Layer>
-                    </Stage>
+                            </Layer>
+                        </Stage>
+                    </div>
                 </div>
             </div>
         </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -11,14 +11,19 @@ export const ACTUAL_DATA = {
             type: 'core',
             name: 'Core stage 1',
             color: '#ff00ff',
+            start_at: 3,
             stages: [
                 {
                     id: '00',
                     type: 'stage',
                     name: 'Aboba',
+                    start_at: 5,
+                    length: 5,
                     tasks: [
                         {
                             id: 'task_01_1',
+                            start_at: 3,
+                            length: 3,
                         },
                     ],
                     stages: [
@@ -26,13 +31,24 @@ export const ACTUAL_DATA = {
                             id: '000',
                             type: 'stage',
                             name: 'Test stage',
+                            start_at: 7,
+                            length: 5,
                         },
                         {
                             id: '001',
                             type: 'milestone',
                             name: 'Milestone 1',
+                            start_at: 8,
+                            length: 1,
                         },
                     ],
+                },
+            ],
+            tasks: [
+                {
+                    id: 'task_0_1',
+                    start_at: 3,
+                    length: 5,
                 },
             ],
         },
@@ -41,19 +57,26 @@ export const ACTUAL_DATA = {
             type: 'core',
             name: 'Core stage 2',
             color: '#ffff00',
+            start_at: 9,
             stages: [
                 {
                     id: '10',
                     type: 'stage',
-                    name: 'Kak dela',
+                    name: 'Stage 10',
+                    start_at: 9,
+                    length: 5,
                     stages: [
                         {
                             id: '100',
                             type: 'stage',
                             name: 'Tak vot',
+                            start_at: 10,
+                            length: 5,
                             tasks: [
                                 {
                                     id: 'task_100_1',
+                                    start_at: 11,
+                                    length: 5,
                                 },
                             ],
                         },
@@ -61,117 +84,10 @@ export const ACTUAL_DATA = {
                             id: '101',
                             type: 'milestone',
                             name: 'Milestone #2',
+                            start_at: 11,
+                            length: 2,
                         },
                     ],
-                },
-            ],
-        },
-    ],
-};
-
-export const initialData = {
-    stages: [
-        {
-            id: 0,
-            name: 'Stage #1',
-            tasks: [
-                {
-                    id: 11,
-                    name: 'Task #11',
-                    start_at: 3,
-                    length: 3,
-                    color: '#A9E5BB',
-                    percent: 100,
-                },
-                {
-                    id: 12,
-                    name: 'Task #12',
-                    start_at: 6,
-                    length: 3,
-                    color: '#F72C25',
-                    percent: 100,
-                },
-            ],
-        },
-        {
-            id: 1,
-            name: 'Stage #2',
-            tasks: [
-                {
-                    id: 21,
-                    name: 'Task #21',
-                    start_at: 7,
-                    length: 3,
-                    color: '#A76D60',
-                    percent: 0,
-                },
-                {
-                    id: 22,
-                    name: 'Task #22',
-                    start_at: 9,
-                    length: 12,
-                    color: '#601700',
-                    percent: 70,
-                },
-            ],
-        },
-        {
-            id: 2,
-            name: 'Stage #3',
-            tasks: [
-                {
-                    id: 31,
-                    name: 'Task #31',
-                    start_at: 10,
-                    length: 5,
-                    color: '#96CDFF',
-                    percent: 50,
-                },
-                {
-                    id: 32,
-                    name: 'Task #32',
-                    start_at: 15,
-                    length: 3,
-                    color: '#DBBADD',
-                    percent: 20,
-                },
-                {
-                    id: 33,
-                    name: 'Task #33',
-                    start_at: 20,
-                    length: 6,
-                    color: '#BE92A2',
-                    percent: 70,
-                },
-            ],
-        },
-        {
-            id: 3,
-            name: 'Stage #4',
-            tasks: [
-                {
-                    id: 41,
-                    name: 'Task #41',
-                    start_at: 21,
-                    length: 3,
-                    color: '#88B7B5',
-                    percent: 90,
-                },
-                {
-                    id: 42,
-                    name: 'Task #42',
-                    start_at: 25,
-                    length: 6,
-                    color: '#449DD1',
-                    percent: 15,
-                },
-                {
-                    id: 43,
-                    name: 'Task #43',
-                    start_at: 25,
-                    length: 6,
-                    color: '#449DD1',
-                    percent: 25,
                 },
             ],
         },
